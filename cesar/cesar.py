b6a1c58e5e6ee078993c84a8e8dfeca682361473
goronc <108907603+goronc@users.noreply.github.com>
2023-03-12 23:32:53 +1100

    Update cesar.py

--- cesar/cesar.py
+++ cesar/cesar.py
@@ -1,3 +1,7 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 def chiffrer(chaine,decalage):  
@@ -32,13 +36,51 @@ def dechiffrer(chaine,decalage):
                 res += alphabet[j-decalage] 
     return res
 
+def frequence_lettre(chaine):
+    dico = {}
+    for i in range(len(chaine)):
+        if chaine[i] in dico.keys():
+            dico[chaine[i]] += 1 
+        else:
+            dico[chaine[i]] = 1 
+    return dico
+
+def max_val_dico(dico):
+    liste_keys = list(dico.keys())
+    liste_keys.remove(" ")
+    liste_value = list(dico.values())
+    indice = 0
+    for i in range(len(liste_value)):
+        if liste_value[i] > liste_value[indice]:
+            indice = i
+    return liste_keys[indice]
+
+
+def decrypter(chaine,alphabet):
+    e_chifrer = max_val_dico(frequence_lettre(chaine))
+    print(e_chifrer) 
+    return 0
 
 # Test ATTENTION carractere spéciaux non compatible
 # 2 possibilitées a choisir pour les caracteres spéciaux  soit les rajoutez a la fin de l'alphabet de reference (abc..xyz!?/:;.?,) soit les laissez tel quel comme les espaces
-x = "hello world"
-y = 5
+x = "a force daller en avant il parvint au point ou le brouillard de la fusillade devenait  transparent si bien que les tirailleurs de la ligne ranges et a laffut derriere leur levee de paves et les tirailleurs de la banlieue masses a langle de la rue se montrerent soudainement  quelque chose qui remuait dans la fumee au moment ou gavroche debarrassait de ses cartouches un sergent gisant pres dune borne une balle frappa le cadavre"
+y = 3
 
-x = chiffrer(x,y)
-print(x)
-x = dechiffrer(x,y)
-print(x)
+z = chiffrer(x,y)
+
+w = dechiffrer(z,y)
+
+print(w == x)
+freq = frequence_lettre(z)
+max_val_dico(frequence_lettre(x))
+decrypter(z,alphabet)
+
+# fig, ax = plt.subplots(figsize=(5, 2.7), layout='constrained')
+# categories = [i for i in freq.keys()]
+# ax.set_title("Fréquence des lettre dans la chaine de caractere")
+# ax.set_ylabel("Nb répétition dans la chaine")
+# ax.set_xlabel("Lettres")
+
+# ax.bar(categories,  freq.values())
+# plt.show()
+ 
